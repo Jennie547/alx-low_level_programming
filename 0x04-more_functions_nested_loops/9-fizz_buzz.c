a25bab2fc2c081d1b935a9ac9c3954901ea58e46
Jennie547 <jessica4edigin@gmail.com>
2023-06-22 13:37:54 -0700

    hattery

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - show the FizzBuzz programs
+ * return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 ==0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0);
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		if ( n != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
