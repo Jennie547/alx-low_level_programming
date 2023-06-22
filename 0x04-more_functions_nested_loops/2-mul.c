4e5f2eb71c36cd642f4c5b7a255532c55809eb33
Jennie547 <jessica4edigin@gmail.com>
2023-06-22 01:41:13 -0700

    Larry king

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of both digits 
+ * @a: the first num
+ * @b: the multiplier
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
