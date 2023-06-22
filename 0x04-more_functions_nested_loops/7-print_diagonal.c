f9a0cd688d17fabe8f514c36c27180a4943fe567
Jennie547 <jessica4edigin@gmail.com>
2023-06-22 12:58:08 -0700

    Java

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - construct a diagonal across the page
+ * @n: number of times the slant line will appear
+ * return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int c, d;
+
+		for (c = 0; c < n; c++)
+		{
+			for (d = 0; d < n; d++)
+			{
+				if (d ==c)
+					_putchar('\\');
+				else if (d < c)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
