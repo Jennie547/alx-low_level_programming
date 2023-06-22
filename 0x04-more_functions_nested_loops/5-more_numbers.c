775e648773f0109ba3406be38e9807ebcec338d9
Jennie547 <jessica4edigin@gmail.com>
2023-06-22 12:26:50 -0700

    Father

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - return series of numbers
+ * return: ten rolls of the numbers from 1 to 14
+ */
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (d = 0; d < 14; d++)
+		{
+			if (d > 9)
+			{
+				_putchar((d / 10) + '0');
+			}
+			_putchar((d % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
