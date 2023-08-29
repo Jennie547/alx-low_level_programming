d67ec7453235c0cc96e8603391421ebe213f0d1a
Jennie547 <jessica4edigin@gmail.com>
2023-08-29 11:31:35 -0700

    second commit

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,25 @@
-#in
+#include "lists.h"
+
+/**
+ * add_nodeint - allows for a new node
+ * @n: node data
+ * @head: pointer
+ *
+ * Return: NULL when failure occurs or new node ptr
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *my_ptr;
+
+	my_ptr = malloc(sizeof(listint_t));
+	if (!my_ptr)
+	{
+		return (NULL);
+	}
+	my_ptr->n = n;
+	my_ptr->next = *head;
+	*head = my_ptr;
+
+	return (my_ptr);
+}
