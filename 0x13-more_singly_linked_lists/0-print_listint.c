edf0ace79e1162537a1edc83bf2dca937a46fbbc
Jennie547 <jessica4edigin@gmail.com>
2023-08-29 11:15:28 -0700

    first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count_nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		count_nodes++;
 		h = h->next;
+		count_nodes++;
 	}
 
 	return (count_nodes);
